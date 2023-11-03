1f0196fd157ab78c4eba49b34391ad4ee95176dd
SirreaReeves <reevessirrea@gmail.com>
2023-11-03 01:52:46 -0400

    handle event function

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,28 @@
 import './styles.css';
-
 import palettes from './palettes.json'
+
+
+const makePalette = () => {
+    return `<div>${obj["palette-title"]}</div>`
+    
+}
+
+const handleEvent = (e) => {
+    e.preventDefault();
+    const form = document.querySelector('#palette-form');
+    const formData = new FormData(form);
+
+    const obj = Object.fromEntries(formData);
+    
+    const li = document.createElement('li');
+    li.innerHTML = makePalette(obj);
+    // const li = makePalette(obj);
+    const ul = document.querySelector('#palette-list');
+    ul.appendChild(li)
+};
+
+const button = document.querySelector('#button');
+button.addEventListener('click', handleEvent);
+
+
 console.log(palettes);
