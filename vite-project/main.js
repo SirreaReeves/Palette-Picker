27fb31df24be4c43d79133f2579c63dc7ff7eb7d
SirreaReeves <reevessirrea@gmail.com>
2023-11-05 00:12:59 -0400

    html and js updated

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,28 +1,34 @@
-import './styles.css';
-import palettes from './palettes.json'
+import palettes from './palettes.json' assert {type: 'json'};
+console.log(palettes); 
 
-
-const makePalette = () => {
+const makePalette = (obj) => {
     return `<div>${obj["palette-title"]}</div>`
-    
-}
+};
 
-const handleEvent = (e) => {
+const button = document.querySelector('#form-button');
+const handleEvent = (event) => {
     e.preventDefault();
     const form = document.querySelector('#palette-form');
     const formData = new FormData(form);
-
     const obj = Object.fromEntries(formData);
     
     const li = document.createElement('li');
     li.innerHTML = makePalette(obj);
-    // const li = makePalette(obj);
     const ul = document.querySelector('#palette-list');
-    ul.appendChild(li)
+    ul.appendChild(li);
 };
 
-const button = document.querySelector('#button');
 button.addEventListener('click', handleEvent);
 
+const copybutton = document.querySelector('.copy-button');
+const copy = (event) => {
+    const hexcode = document.querySelector('#hexcode');
+    
+    hexcode.select();
+    hexcode.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(hexcode.value);
+    copybutton.textContent = 'Copied!';
 
-console.log(palettes);
+};
+
+copybutton.addEventListener('click', copy);
